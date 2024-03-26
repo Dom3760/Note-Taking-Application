a8c6eaf649b77a84a4ede1173b176db60e68ecc5
Dom3760 <142520274+Dom3760@users.noreply.github.com>
2024-03-26 10:04:13 -0700

    Add files via upload

--- NoteTakingApplication/Note Taking Application/ContentView.swift
+++ NoteTakingApplication/Note Taking Application/ContentView.swift
@@ -7,24 +7,34 @@
 
 import SwiftUI
 
+struct Note {
+    var title: String
+    var body: String
+}
+
 struct ContentView: View {
+    @State private var notes:[Note] = []
+    
     var body: some View {
-        VStack {
-            Text("Notes:")
-                .font(.largeTitle)
-                .position(x: 70.0, y: 50.0)
-            NavigationView{
-                NavigationLink(destination: newNoteView().navigationBarBackButtonHidden(false)) {
-                    //adds text to the button
-                    Text("Add Item")
-                        //adds padding, background color, text color, rounding corners,and custom font
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                        .font(.custom("MadimiOne-Regular", size: 30))
-                        .frame(width: 400, height: 100, alignment: .center)
-                        .position(x: 200, y: 350)
+        NavigationView{
+            VStack {
+                Text("Notes:")
+                    .font(.largeTitle)
+//                    .position(x: 70.0, y: 50.0)
+                Rectangle()
+                    .fill(Color(red: 240/255, green: 240/255, blue: 240/255))
+//                                           .frame()
+                NavigationLink(destination: newNoteView(noteArr: $notes).navigationBarBackButtonHidden(false)) {
+                        //adds text to the button
+                        Text("Add Item")
+                            //adds padding, background color, text color, rounding corners,and custom font
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .font(.custom("MadimiOne-Regular", size: 30))
+                            .frame(width: 400, height: 100, alignment: .center)
+//                            .position(x: 200, y: 350)
                 }
             }
         }
--- NoteTakingApplication/Note Taking Application/newNoteView.swift
+++ NoteTakingApplication/Note Taking Application/newNoteView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct newNoteView: View {
+    @State var Title1: String = ""
+    @State var Body1: String = ""
+    @Binding var noteArr: [Note]
     var body: some View {
-        VStack {
-            
+        NavigationView{
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color(red: 240/255, green: 240/255, blue: 240/255))
+                        .frame(height: 80)
+                    TextField("Title", text: $Title1)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                }
+                ZStack{
+                    Rectangle()
+                        .fill(Color(red: 240/255, green: 240/255, blue: 240/255))
+                    TextEditor(text: $Body1)
+                        .frame(height: 500, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                }
+                Button(action: ), label: ( {
+                    Text("Button")
+                })
+//                NavigationLink(destination: newNoteView().navigationBarBackButtonHidden(false)) {
+//                    Text("Add Item")
+//                        .padding()
+//                        .background(Color.blue)
+//                        .foregroundColor(.white)
+//                        .cornerRadius(10)
+//                        .font(.custom("MadimiOne-Regular", size: 20))
+//                        .frame(width: 400, height: 100, alignment: .center)
+//                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    newNoteView()
+    newNoteView(noteArr: .constant([]))
 }
